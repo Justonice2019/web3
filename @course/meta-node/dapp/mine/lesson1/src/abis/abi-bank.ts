9d6c0df20ba46fb14ca16addd5aec98ce56bd060
俞连强 <1175409441@qq.com>
2026-01-26 10:40:58 +0800

    feat: _

---
+++ @course/meta-node/dapp/mine/lesson1/src/abis/abi-bank.ts
@@ -0,0 +1,125 @@
+export const abiBank1 = [
+  {
+    "stateMutability": "payable",
+    "type": "fallback"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "balances",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "deposit",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "getBalance",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "withdraw",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "stateMutability": "payable",
+    "type": "receive"
+  }
+] as const
+
+export const abiBank2 = [
+  {
+    "stateMutability": "payable",
+    "type": "fallback"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "balances",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "deposit",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "getBalance",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "withdraw",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "stateMutability": "payable",
+    "type": "receive"
+  }
+] as const
--- @course/meta-node/dapp/mine/lesson1/src/pages/wagmi/index.tsx
+++ @course/meta-node/dapp/mine/lesson1/src/pages/wagmi/index.tsx
@@ -1,30 +1,63 @@
 import {useCallback, useState} from 'react'
-import {useReadContract, useAccount, usePublicClient} from 'wagmi'
-import {abiBank} from '../../abis/abi'
+import {useReadContract, useAccount, usePublicClient, useReadContracts} from 'wagmi'
+import {abiBank1, abiBank2} from '../../abis/abi-bank'
 
 export default function Page() {
+  const [balance, setBalance] = useState<bigint | null>(null)
+  const [balances, setBalances] = useState<Array<bigint | null>>([])
+
   const account = useAccount()
   const publicClient = usePublicClient()
 
   const result = useReadContract({
-    abi: abiBank,
+    abi: abiBank1,
     address: '0x947a31dD8cff2F23C9D188079dD1a3E3471D8903',
     functionName: 'getBalance',
     account: account.address as `0x${string}`,
     query: {
-      enabled: true, // 只有点击按钮后才启用
+      enabled: Boolean(account.address),
     }
   })
-  const [balance, setBalance] = useState<bigint | null>(null)
+  // const result2 = useReadContracts({
+  //   contracts: [
+  //     {
+  //       abi: abiBank1,
+  //       address: '0x947a31dD8cff2F23C9D188079dD1a3E3471D8903',
+  //       functionName: 'getBalance',
+  //     }
+  //   ],
+  //   account: account.address as `0x${string}`,
+  // })
+  // console.log(result2)
 
   const onGetBalance = useCallback(async () => {
     const data = await publicClient?.readContract({
       address: '0x947a31dD8cff2F23C9D188079dD1a3E3471D8903',
-      abi: abiBank,
+      abi: abiBank1,
       functionName: 'getBalance',
       account: account.address as `0x${string}`,
     })
+    console.log(data)
     setBalance(data as bigint);
+  }, [account.address, publicClient])
+
+  const onGetMultiBalance = useCallback(async () => {
+    const data = await publicClient?.multicall({
+      contracts: [
+        {
+          address: '0x947a31dD8cff2F23C9D188079dD1a3E3471D8903',
+          abi: abiBank1,
+          functionName: 'getBalance',
+        }
+      ],
+      account: account.address,
+    })
+    const newBalances = [
+        ...balances
+    ]
+    const balancesList = data?.map(d => d.result as bigint) as Array<bigint>
+    newBalances.push(...balancesList)
+    setBalances(newBalances)
   }, [account.address, publicClient])
 
   return (
@@ -32,8 +65,14 @@ export default function Page() {
         {account.address && <div>
             <div>当前地址: {account.address}</div>
             <div>余额: {result.data as bigint}</div>
-            <span>余额: {balance};</span>
-            <button onClick={onGetBalance}>获取余额</button>
+            <div>
+                <span>余额: {balance};</span>
+                <button onClick={onGetBalance}>获取余额(单个合约)</button>
+            </div>
+            <div>
+                <span>余额: {balances?.join(', ')};</span>
+                <button onClick={onGetMultiBalance}>获取余额(多个合约)</button>
+            </div>
         </div>}
       </div>
   )
